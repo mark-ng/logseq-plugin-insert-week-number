6472aec8153352612f75bf2ceb6f3765341712e4
Mark Ng <stngad@connect.ust.hk>
2023-01-02 01:21:35 +0800

    Implement insertWeekNumber Slash Command in any journal page

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 import '@logseq/libs';
 
-//Inputs 5 numbered blocks when called
-async function insertSomeBlocks (e) {
-  console.log('Open the calendar!')
-  let numberArray = [1, 2, 3, 4, 5]
-  for (const number in numberArray){
-  logseq.App.showMsg("Function has been run")
-  logseq.Editor.insertBlock(e.uuid, `This is block ${numberArray[number]}`, {sibling: true})}
-
+Date.prototype.getWeek = function () {
+  var target  = new Date(this.valueOf());
+  var dayNr   = (this.getDay() + 6) % 7;
+  target.setDate(target.getDate() - dayNr + 3);
+  var firstThursday = target.valueOf();
+  target.setMonth(0, 1);
+  if (target.getDay() != 4) {
+      target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
   }
-  
+  return 1 + Math.ceil((firstThursday - target) / 604800000);
+}
+
+Date.prototype.getWeekYear = function() {
+  var date = new Date(this.getTime());
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  return date.getFullYear();
+}
+
+function parseDate(dateString) {
+  return new Date(dateString.substring(0, 4), 
+                      dateString.substring(4, 6) - 1, 
+                      dateString.substring(6));
+}
 
 const main = async () => {
-  console.log('plugin loaded');
-  logseq.Editor.registerSlashCommand('insertBlocks', async (e) => {
-    insertSomeBlocks(e)
-  }
-    
-  )}
+
+  logseq.Editor.registerSlashCommand('insertWeekNumber', async (e) => {
+    const currentPage = await logseq.Editor.getCurrentPage()
+    if (currentPage && currentPage['journal?']) {
+      const journalDay = currentPage.journalDay + ""
+
+      const date = parseDate(journalDay)
+      const week = date.getWeek()
+      const weekYear = date.getWeekYear()
+
+      logseq.Editor.updateBlock(e.uuid, `[[Year${weekYear}/Week${week}]]`)
+    }
+  })
+  
+}
 
 logseq.ready(main).catch(console.error);
